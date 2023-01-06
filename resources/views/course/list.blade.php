d39597fe20f383faa75325fcab7e4ab33b59aa0a
emmoki <emmanuelnuswantoro@gmail.com>
2023-01-06 10:59:07 +0700

    course list page

--- resources/views/course/list.blade.php
+++ resources/views/course/list.blade.php
@@ -1,14 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    @forelse ($courses as $course)
-        <h1>{{ $course->name }}</h1>
+@extends('template')
+
+@section('content')
+<style>
+    .card{
+        cursor: pointer;
+        transition: 0.3s;
+    }
+    .card:hover{
+        box-shadow: 0 0px 10px rgba(13,110,253);
+    }
+
+    .add-course{
+        height: 50px; 
+        text-decoration: none;
+        border-radius: 10px;
+    }
+    .card-text{
+        /* text-overflow: ellipsis; */
+        overflow: hidden;
+        display: -webkit-box !important;
+        -webkit-line-clamp: 8;
+        -webkit-box-orient: vertical; 
+        white-space: normal;
+    }
+</style>
+<div class="container">
+    <div class="row bg-primary d-flex justify-content-center align-items-center mt-5" style="height: 18rem; border-radius: 10px;">
+        <div>
+            <form class="d-flex flex-column mx-5" role="search">
+                <label for="">
+                    <h1 class="text-white">
+                        What do you want to learn?
+                    </h1>
+                </label>
+                <input class="form-control" type="search" placeholder="Search Courses here..." aria-label="Search">
+                <button class="btn btn-outline-success text-white mt-2" style="border: 2px solid white;" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+    <div class="row text-primary mt-3">
+        <h1>Course List</h1>
+    </div>
+    <div class="row" style="min-height: 980px">
+        @forelse ($courses as $course)
+        <a href="/courses/{{$course->id}}" class="card m-2 text-dark" style="width: 18rem; height: 20rem; text-decoration: none;">
+            <div class="card-body">
+                <h5 class="card-title">{{ $course->name }}</h5>
+                <p class="card-text" style="height: 68%; overflow: hidden;">{{ $course->description }}</p>
+                <!-- <button href="#" class="btn btn-primary d-flex justify-content-center" style="width: 100%;">Enroll</button> -->
+                <form action="/enroll/{{ $course->id }}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn btn-primary" style="width: 100%;">Enroll</button>
+                </form>
+            </div>
+        </a>
+        <!-- <h1>{{ $course->name }}</h1>
         <p>{{ $course->description }}</p>
         <div>
             <form action="/enroll/{{ $course->id }}" method="POST">
@@ -16,9 +62,13 @@
                 <button type="submit">Enroll</button>
             </form>
         </div>
-        <hr>
-    @empty
-        <h1>No courses</h1>
-    @endforelse
-</body>
-</html>
+        <hr> -->
+        @empty
+            <h3 class="d-flex justify-content-center align-items-center" style="filter:invert(80%); height: 20rem;">No course that has been added by the teacher</h3>
+        @endforelse
+    </div>
+    
+
+</div>
+
+@endsection
