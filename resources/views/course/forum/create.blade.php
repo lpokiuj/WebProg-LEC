1b911e1e6ba5eb6384b1439c123d8a8107ac8b47
emmoki <emmanuelnuswantoro@gmail.com>
2023-01-06 10:58:38 +0700

    Forum Page

--- resources/views/course/forum/create.blade.php
+++ resources/views/course/forum/create.blade.php
@@ -1,39 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('template')
 
-    @if ($errors->any())
-        <h4>Error</h4>
-        <ul class="">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+@section('content')
+<div class="container" style="min-height: 90vh;">
+    <div class="row text-primary mt-3">
+        <h1>Create Forum</h1>
+    </div>
+    <div class="row text-danger">
+        @if ($errors->any())
+            <ul class=""> 
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
+    <div class="row">
+        <form action="/courses/{{ $id }}/forums" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Title</label>
+                <input type="type" name="title" class="form-control" id="exampleFormControlInput1" placeholder="Course Title">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Course Description</label>
+                <textarea name="description" class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="formFile" class="form-label">Default file input example</label>
+                <input class="form-control" type="file" id="formFile" name="attachment">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Objective</label>
+                <div id="inputObjective">
+                    <div class="d-flex" >
+                        <input type="type" name="objective[]" class="form-control" placeholder="Objectives">
+                    </div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <button type="button" class="btn btn-success" id="addObjective" style="width: 100%;">Add more objective</button>
+            </div>
+            <button type="submit" class="btn btn-primary mb-5" style="width: 100%">Submit</button>
+        </form>
+    </div>
+</div>
+<script type="text/javascript">
+        $(document).ready(function(){
+            $('#addObjective').on('click',function(){
+                let line=`
+                <div class="d-flex mt-2">
+                    <input type="type" name="objective[]" class="form-control" placeholder="Objectives">
+                    <button type="button" class="btn btn-danger ms-2" id="delete">Delete</button>
+                </div>
+                `;
+                $('#inputObjective').append(line);
+            })
+        })
 
-    <form action="/courses/{{ $id }}/forums" method="POST" enctype="multipart/form-data">
-        @csrf
-        <label for="title">Title: </label>
-        <input type="text" name="title" id="">
-        <label for="description">Description: </label>
-        <input type="text" name="description">
-        <label for="attachment">Attachment: </label>
-        <input type="file" name="attachment">
-
-        <div>
-            <label for="objective">Objective</label>
-            <input type="text" name="objective[]">
-            <input type="text" name="objective[]">
-            <input type="text" name="objective[]">
-        </div>
-        <button type="submit">Submit</button>
-    </form>
-
-</body>
-</html>
+        $(document).on('click', '#delete', function(){
+            $(this).parent().remove();
+        })
+    </script>
+@endsection
