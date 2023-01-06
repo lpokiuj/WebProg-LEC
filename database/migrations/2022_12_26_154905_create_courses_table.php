ac7a3b552059dd6070abd6fa197beedbcc13e27a
emmoki <emmanuelnuswantoro@gmail.com>
2023-01-06 11:01:51 +0700

    adding more capacity on description attribute for course

--- database/migrations/2022_12_26_154905_create_courses_table.php
+++ database/migrations/2022_12_26_154905_create_courses_table.php
@@ -16,7 +16,7 @@ class CreateCoursesTable extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->string('description' , 5000);
             $table->timestamps();
         });
     }
